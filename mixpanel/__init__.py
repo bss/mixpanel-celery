4b56c87a9f1ffed4a4a9b9c594ac50fed7c35eec
Aron Griffis <aron@arongriffis.com>
2012-11-12 23:21:46 -0500

    Update mixpanel/__init__.py with my maintainership for now

--- mixpanel/__init__.py
+++ mixpanel/__init__.py
@@ -1,10 +1,13 @@
 """Asynchronous event tracking for Mixpanel"""
 
-VERSION = (0, 5, 0, '')
+VERSION = (0, 6, 0, '')
 
 __version__ = ".".join(map(str, VERSION[:-1]))
 __release__ = ".".join(map(str, VERSION))
-__author__ = "Wes Winham"
-__contact__ = "wes@policystat.com"
-__homepage__ = "http://github.com/winhamwr/mixpanel-celery/"
+#__author__ = "Wes Winham"
+#__contact__ = "wes@policystat.com"
+#__homepage__ = "http://github.com/winhamwr/mixpanel-celery/"
+__author__ = "Aron Griffis"
+__contact__ = "aron@arongriffis.com"
+__homepage__ = "http://github.com/agriffis/mixpanel-celery/"
 __docformat__ = "restructuredtext"
